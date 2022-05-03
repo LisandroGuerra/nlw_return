6d14643b8800853d315be546f74298cf8e62774f
Lisandro Guerra <lisandro.digital@gmail.com>
2022-05-03 20:54:39 -0300

    stage1 TaleWindCSS first use

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -4,13 +4,16 @@ interface ButtonProps {
 }
 
 function Button(props: ButtonProps) {
-  return <button>{props.text ?? 'Default'}</button>
+  return <button
+    className="bg-orange-600 text-orange-100 
+    p-2 rounded px-4 h-10
+    hover:bg-orange-700 transition-colors">{props.text ?? 'Default'}</button>
 }
 
 function App() {
 
   return (
-    <div>
+    <div className="flex gap-2">
       <Button text="Send" />
       <Button text="Receive" />
       <Button text="Delete" />
