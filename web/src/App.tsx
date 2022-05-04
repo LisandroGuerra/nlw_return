8228a210329261e124973a03cfcbe383c9952fa8
Lisandro Guerra <lisandro.digital@gmail.com>
2022-05-03 21:39:24 -0300

    stage1 install icons Phosphor and create the button feedback

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,25 +1,5 @@
+import { Widget } from "./components/Widget";
 
-interface ButtonProps {
-  text?: string;
+export function App() {
+  return <Widget />
 }
-
-function Button(props: ButtonProps) {
-  return <button
-    className="bg-orange-600 text-orange-100 
-    p-2 rounded px-4 h-10
-    hover:bg-orange-700 transition-colors">{props.text ?? 'Default'}</button>
-}
-
-function App() {
-
-  return (
-    <div className="flex gap-2">
-      <Button text="Send" />
-      <Button text="Receive" />
-      <Button text="Delete" />
-      <Button />
-    </div>
-  )
-}
-
-export default App
