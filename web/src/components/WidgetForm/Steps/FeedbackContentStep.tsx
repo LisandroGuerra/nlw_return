b09f7a30ae878dfe199a3a9a462313a68d58802a
Lisandro Guerra <lisandro.digital@gmail.com>
2022-05-08 11:04:12 -0300

    Stage5 fixing send button behavior  and start API

--- web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -3,6 +3,8 @@ import { ArrowLeft, Camera } from "phosphor-react"
 import { FeedbackType, feedbackTypes } from ".."
 import { CloseButton } from "../../CloseButton"
 import { ScreenshotButton } from "../ScreenshotButton"
+import { api } from "../../../lib/api"
+import { Loading } from "../../Loading"
 
 interface FeedbackContentStepProps {
     feedbackType: FeedbackType
@@ -15,14 +17,19 @@ export function FeedbackContentStep(
 
     const [screenshot, setScreenshot] = useState<string | null>(null)
     const [comment, setComment] = useState<string>('')
+    const [isSendingFeedback, setIsSendingFeedback] = useState(false)
 
     const feedbackTypeInfo = feedbackTypes[feedbackType]
 
-    function handleSubmitFeedback(event: FormEvent) {
+    async function handleSubmitFeedback(event: FormEvent) {
         event.preventDefault()
-        console.log({
-            screenshot,
+
+        setIsSendingFeedback(true)
+
+        await api.post('/feedbacks', {
+            type: feedbackType,
             comment,
+            screenshot,
         })
 
         onFeedbackSent()
@@ -72,7 +79,7 @@ export function FeedbackContentStep(
 
                     <button
                         type="submit"
-                        disabled={comment.length < 5}
+                        disabled={comment.length < 5 || isSendingFeedback}
                         className="p-2 bg-brand-500 rounded-md border-transparent
                         flex-1 flex justify-center items-center 
                         text-sm hover:bg-brand-300 transition-colors
@@ -80,7 +87,7 @@ export function FeedbackContentStep(
                         focus:ring-offset-zinc-900 focus:ring-brand-500
                         disabled:opacity-50 disabled:hover:bg-brand-500"
                     >
-                        Enviar feedback
+                        {isSendingFeedback ? <Loading /> : 'Enviar feedback'}
                     </button>
 
                 </footer>
